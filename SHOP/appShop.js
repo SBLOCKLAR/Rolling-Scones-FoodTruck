96116269e29bc231ee67fe72dcb54d6e3f6b985e
Sarah Beth Locklar <110067009+SBLOCKLAR@users.noreply.github.com>
2023-03-04 18:56:04 -0500

    Shop Updates
    Fixed product stylings
    Updated selectors.

--- SHOP/appShop.js
+++ SHOP/appShop.js
@@ -6,3 +6,12 @@ const links = document.querySelectorAll('.pageLinks');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
 });
+
+const cart = document.querySelector('#cart');
+const addToCart = document.querySelectorAll('.addToCart');
+const product = [{
+    'id': "",
+    'price': "",
+    'size': "",
+    'qty': "",
+}];
